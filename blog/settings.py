dd54d95b6c88f04018b8f39737522f91c8907793
dyopp92 <44453174+dyopp92@users.noreply.github.com>
2021-03-04 11:54:31 -0600

    db postgres

--- blog/settings.py
+++ blog/settings.py
@@ -103,12 +103,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
